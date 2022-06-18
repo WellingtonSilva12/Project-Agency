ec71a97568e20ef98d1e6799ed3250d3412badde
WellingtonSilva12 <wellingtonsilva180g@gmail.com>
2022-06-18 17:14:18 -0300

    functional mobile nav menu with javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+const ul = document.querySelector('nav ul')
+const fixed = document.querySelector('.main')
+const menuBtn = document.querySelector('.menu-btn i')
+
+function menuShow() {
+  if (ul.classList.contains('')) {
+    ul.classList.toggle('open')
+    fixed.classList.toggle('open')
+  } else {
+    ul.classList.toggle('open')
+    fixed.classList.toggle('open')
+  }
+}
