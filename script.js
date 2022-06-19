5b4c99cc014af67602d4f8626d4e346ae57d2e90
WellingtonSilva12 <wellingtonsilva180g@gmail.com>
2022-06-19 15:54:56 -0300

    Making the application responsive and accessible

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const ul = document.querySelector('nav ul')
 const fixed = document.querySelector('.main')
-const menuBtn = document.querySelector('.menu-btn i')
+const menuBtn = document.querySelector('.menu-btn')
+const menuNav = document.querySelector('nav')
 
 function menuShow() {
-  if (ul.classList.contains('')) {
+  if (ul.classList.contains('open')) {
     ul.classList.toggle('open')
     fixed.classList.toggle('open')
+    menuNav.classList.toggle('open')
   } else {
     ul.classList.toggle('open')
     fixed.classList.toggle('open')
+    menuNav.classList.toggle('open')
   }
 }
